b625e6618638a2f8b37f29c1f639e49766cf5f15
TranTanMinh <tanminhtran168@gmail.com>
2020-11-03 22:36:02 +0700

    Add comment to Factorial.cpp

--- Factorial digit sum.cpp
+++ Factorial digit sum.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// hàm thực hiện nhân số n! với số n + 1 để được số (n+1)!
 void multiply(vector<int> &v, int x) {
     int carry = 0, res;
     int size = v.size();
@@ -15,15 +16,18 @@ void multiply(vector<int> &v, int x) {
     }
 }
 
+// hàm tìm tổng các chữ số của n
 int findSumOfDigits(int n) {
     vector<int> v;
     v.push_back(1);
 
+    // thực hiện tính số n!
     for (int i = 1; i <= n; i++)
         multiply(v, i);
 
     int sum = 0;
     int size = v.size();
+    // tính tổng các chữ số của n
     for (int i = 0 ; i < size ; i++)
         sum += v[i];
     return sum;
